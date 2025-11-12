3c3bb5f7551d95c6addb90131a01ecd3e30a5131
Maxi <maximiliano.kreiner@pbs-arg.com>
2025-11-12 08:20:50 -0300

    Fix name en PlansController

--- app/Http/Controllers/PlansController.php
+++ app/Http/Controllers/PlansController.php
@@ -10,7 +10,7 @@ class PlansController extends Controller
 
     public function index(Request $request)
     {
-        $sql = "SELECT pla_id as id,pla_name as bane ,case when (now() < coalesce(pla_enddate,'2099-12-31')) then 'ALTA' else 'BAJA' end as status FROM pbs_rating.ra_plans";
+        $sql = "SELECT pla_id as id,pla_name as name ,case when (now() < coalesce(pla_enddate,'2099-12-31')) then 'ALTA' else 'BAJA' end as status FROM pbs_rating.ra_plans";
         $results = DB::select($sql);
         return response()->json($results, 200);
     }
